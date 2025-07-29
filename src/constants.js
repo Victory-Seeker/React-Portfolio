580a1004c80645e210caed8ee495cfb2bee17b6c
Victory-Seeker <kundukoustav81@gmail.com>
2025-07-29 23:34:03 +0530

    Update Projects

--- src/constants.js
+++ src/constants.js
@@ -35,6 +35,7 @@ import calLogo from './assets/work_logo/calculator.png';
 import chatApp from './assets/work_logo/chat_app.png';
 import portfolioLogo from './assets/work_logo/portfolio.png';
 import geminiClone from './assets/work_logo/gemini_clone.png';
+import DreamPixels from './assets/work_logo/DreamPixels.png';
 
 
 export const SkillsInfo = [
@@ -120,17 +121,28 @@ export const SkillsInfo = [
   export const projects = [
     {
       id: 0,
-      title: "Quick Chat",
+      title: "TalkHive",
       description:
-        "QuickChat is a real-time messaging platform that allows users to seamlessly chat with each other. Built with a full stack architecture, it features secure user authentication using JWT, real-time communication powered by Socket.IO, and a responsive front-end developed with React. Users can log in, log out, and interact in real-time, with messages updating instantly across clients.",
+        "TalkHive is a real-time messaging platform that allows users to seamlessly chat with each other. Built with a full stack architecture, it features secure user authentication using JWT, real-time communication powered by Socket.IO, and a responsive front-end developed with React. Users can log in, log out, and interact in real-time, with messages updating instantly across clients.",
       image: chatApp,
       tags: ["JavaScript", "React JS", "Node JS", "Express JS", "Socket.io", "JWT", "MongoDB", "Tailwind CSS"],
-      github: "https://github.com/Victory-Seeker/QuickChat",
+      github: "https://github.com/Victory-Seeker/TalkHive",
       webapp: "https://quick-chat-ecru.vercel.app/login",
     },
 
     {
       id: 1,
+      title: "DreamPixels",
+      description:
+        "DreamPixels is a full-stack Text-to-Image Generator web application that enables users to create AI-powered images using the ClipDrop API, secured with JWT authentication and credit-based access. The frontend, built with React.js, Tailwind CSS, and Framer Motion, delivers a smooth, animated user experience, while the backend uses Node.js, Express.js, MongoDB, and Razorpay for secure payments and robust API handling.",
+      image: DreamPixels,
+      tags: ["JavaScript", "React JS", "Node JS", "Express JS", "JWT", "MongoDB", "Tailwind CSS" , "Framer Motion", "Razorpay", "ClipDrop API"],
+      github: "https://github.com/Victory-Seeker/DreamPixels",
+      webapp: "https://dream-pixels-8rjq.vercel.app/",
+    },
+
+    {
+      id: 2,
       title: "Personal Portfolio",
       description:
         "A fully responsive and modern portfolio website showcasing my skills, projects, education. Built using React and Tailwind CSS, it reflects my expertise in frontend development and UI/UX design. The site features smooth animations and a clean layout to provide a seamless browsing experience.",
@@ -141,7 +153,7 @@ export const SkillsInfo = [
     },
 
     {
-      id: 2,
+      id: 3,
       title: "Gemini Clone",
       description:
         "A functional clone of Google’s Gemini chatbot, built using the Gemini free API. This real-time AI assistant mimics conversational capabilities, enabling users to ask questions, get contextual responses, and interact just like a smart AI chatbot. The project demonstrates integration with external APIs and real-time user input handling with a clean, minimal UI.",
@@ -152,7 +164,7 @@ export const SkillsInfo = [
       webapp: "https://gemini-clone-swart-nu.vercel.app/",
     },
     {
-      id: 3,
+      id: 4,
       title: "Trivia Quiz App",
       description:
         "An interactive quiz application that fetches questions dynamically from the Open Trivia Database API (OpenTDB). Users can test their knowledge across various categories and difficulty levels, receiving immediate feedback for each question. The app ensures a smooth user experience with intuitive design and seamless API integration.",
@@ -162,7 +174,7 @@ export const SkillsInfo = [
       webapp: "https://trivia-quiz-app-hazel.vercel.app/",
     },
     {
-      id: 4,
+      id: 5,
       title: "Scientific Calculator",
       description:
         "A powerful and intuitive scientific calculator application designed to perform a wide range of complex mathematical operations beyond basic arithmetic. Users can compute trigonometric functions like sin, cos, tan, logarithmic calculations, exponentials, and more—all in a sleek and responsive interface.",
